10f35f3ca1fb20013a16afe0ec6c93d78109bdc7
Mikhail Popov <astat.popov@yandex.ru>
2023-10-15 02:42:58 +0300

    Initial Commit

---
+++ ServerAPI/Controllers/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServerAPI.Model;
+
+namespace ServerAPI.Controllers;
+
+[Route("api/[controller]")]
+public class PlayerController: Controller
+{
+    private IEnumerable<Player> Players { get; set; } = new[] { new Player() { Id = 1, Name = "Listik" }};
+
+    /// <summary>
+    /// Returns list of Players
+    /// </summary>
+    /// <returns>Player</returns>
+    [HttpGet(Name = "GetPlayers")]
+    public IEnumerable<Player> GetPlayers()
+    {
+        return Players;
+    }
+    
+    [HttpGet("{id}", Name = "GetPlayerById")]
+    public IEnumerable<Player> GetPlayerById(int id)
+    {
+        return Players.Where(p => p.Id == id);
+    }
+}
---
+++ ServerAPI/Model/Player.cs
@@ -0,0 +1,7 @@
+﻿namespace ServerAPI.Model;
+
+public class Player
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+}
